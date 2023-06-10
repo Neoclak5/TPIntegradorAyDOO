e4979e8ecbb793c3f233287ec58ac8212ef907a6
Neoclak5 <93202124+Neoclak5@users.noreply.github.com>
2023-06-10 12:56:15 -0300

    Add files via upload

---
+++ transportit/src/test/java/com/aydoo/transportit/TransportitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydoo.transportit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
